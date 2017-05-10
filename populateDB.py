248c82846d75695a487fd6584a06dedd6b5d111d
conzty01 <conzty01@luther.edu>
2017-05-09 19:45:51 -0500

    many updates

--- populateDB.py
+++ populateDB.py
@@ -4,11 +4,16 @@ def popResHall(cur):
 	for n in ['Brandt','Olson','Miller','Dieseth','PFABS','Ylvisaker']:
 		cur.execute("INSERT INTO lc_res_hall (name) VALUES ('{}')".format(n))
 
+def popBrandtRA(cur):
+	for n in ["Abby Korenchan", "Anna Dewitt", "Claire Lutter", "Derek Barnhouse", "Elizabeth Myra", "Emily DeJong",
+			  "Emma Deihl", "Jessica Skjonsby", "Kelvin Li", "Luise Johannes","Lukas Phillips","Mason Montuoro","Sam Storts","Tyler Conzett","Vicky Toriallas"]:
+		cur.execute("INSERT INTO lc_resident_assistant (first_name, last_name, hall_id) VALUES ('{}','{}',1)".format(n.split()[0],n.split()[1]))
+
 def main():
 	conn = psycopg2.connect(dbname="conzty01",user="conzty01")
 	cur = conn.cursor()
 	popResHall(cur)
-	
+
 	conn.commit()
 
 main()
--- scheduleServer.py
+++ scheduleServer.py
@@ -1,46 +1,86 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
+import datetime
 import psycopg2
 import sqlalchemy
 import calendar
-import json
 import os
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 Bootstrap(app)
-#conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
+conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
+ct = datetime.datetime.now()
+fDict = {"text_month":calendar.month_name[(ct.month+1)%12], "num_month":(ct.month+1)%12, "year":(ct.year if ct.month <= 12 else ct.year+1)}
+cDict = {"text_month":calendar.month_name[ct.month], "num_month":ct.month, "year":ct.year}
+cc = calendar.Calendar(6) #format calendar so Sunday starts the week
 
 #     -- Views --
 
 @app.route("/")
 def index():
-    c = calendar.Calendar(6)
-    cDict = {"year":2017,
-             "month":5,
-             "Tmonth":"May"}
-
-    return render_template("index.html", calDict=cDict,cal=c.monthdays2calendar(cDict["year"],cDict["month"]))
+    cur = conn.cursor()
+    cur.execute("SELECT id, name, calendar_id FROM lc_res_hall;")
+    return render_template("index.html", hall_list=cur.fetchall())
 
 @app.route("/conflicts")
 def conflicts():
-    pass
+    cur = conn.cursor()
+    cur.execute("SELECT id, first_name, last_name FROM lc_resident_assistant WHERE hall_id = 1 ORDER BY last_name ASC;")
+
+    return render_template("conflicts.html", calDict=fDict, \
+                            cal=cc.monthdays2calendar(fDict["year"],fDict["num_month"]), ras=cur.fetchall())
 
 @app.route("/archive")
 def archive():
-    pass
+    #This will largely be interactive with javascript to ping back and forth years and schedule 'Objects'
+    #Javascript will also be able to reset the view of the calendar below the selector
+    # No forms necessary
+    cur = conn.cursor()
+
+    return render_template("archive.html")
 
 #     -- Functional --
 
-@app.route("/conflicts/parse", methods=["POST"])
-def parseConflicts():
-    pass
+@app.route("/conflicts/p", methods=['POST'])
+def processConflicts():
+    insert_cur = conn.cursor()
+
+    dateList = []
+    for key in request.form:
+        if "day" in key:
+            dateList.append(key.split("y")[-1])
+
+    for d in dateList:
+
+        insert_cur.execute("INSERT INTO lc_conflict (ra_id, date) VALUES ({},TO_DATE('{}-{:>2}-{:>2}','YYYY/MM/DD'));"\
+                            .format(int(request.form["selectRA"]),fDict["year"],fDict["num_month"],d))
+
+    conn.commit()
+
+    if request.form["runScheduler"] == "noRun":
+        cur = conn.cursor()
+        cur.execute("SELECT id, first_name, last_name FROM lc_resident_assistant WHERE hall_id = 1 ORDER BY last_name ASC;")
+        return render_template("conflicts.html", calDict=fDict, \
+                                cal=cc.monthdays2calendar(fDict["year"],fDict["num_month"]), ras=cur.fetchall())
+
+    else:
+        runScheduler()
+        return render_template("index.html", calDict=cDict, \
+                                cal=cc.monthdays2calendar(cDict["year"],cDict["num_month"]))
+
+def runScheduler():
+    return
 
 #     -- api --
 
-@app.route("/api/v1/")
+@app.route("/api/v1/curSchedule")
 def apiSearch():
-    pass
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM lc_schedule WHERE EXTRACT(MONTH FROM month) = EXTRACT(MONTH FROM NOW()) AND
+                                                   EXTRACT(YEAR FROM month) = EXTRACT(YEAR FROM NOW());""")
+
+    return jsonify(cur.fetchall())
 
 if __name__ == "__main__":
     app.run(debug=True)
