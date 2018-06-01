47adc8fab10e9d55508889fc094b5d2c3073d040
conzty01 <conzty01@luther.edu>
2018-06-01 13:16:11 -0500

    Added Unittests for RA Objects Specifically, added testing for equal operation as well as the pseudo random sorting via __lt__

--- tests/RA_test.py
+++ tests/RA_test.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from datetime import date
 from ra_sched import RA
 
@@ -35,6 +36,40 @@ class TestRAObject(unittest.TestCase):
 
     def testGetHallId(self):
         self.assertEqual(self.ra.getHallId(),self.ra.hallId)
+
+    def testEqual(self):
+        ra1 = RA("1","1",1,0,date(2017,1,1))
+        ra2 = RA("1","1",1,0,date(2017,1,1))
+        ra3 = RA("2","2",3,0,date(2017,1,1))
+
+        self.assertEqual(ra1,ra2)
+        self.assertNotEqual(ra1,ra3)
+
+    def testLessThan(self):
+        ra1 = RA("1","1",1,0,date(2017,1,1))
+        ra2 = RA("2","2",2,0,date(2017,1,1))
+        ra3 = RA("3","3",3,0,date(2017,1,1))
+        ra4 = RA("4","4",3,0,date(2017,1,1))
+        ra5 = RA("5","5",3,0,date(2017,1,1))
+
+        # By default, all RAs have 0 points.
+        ra1.addPoints(1)
+        ra2.addPoints(1)
+        ra3.addPoints(5)
+        ra4.addPoints(3)
+        # The general, expected outcome is:
+        #     ra5, ra1 or ra2, ra4, ra3
+
+        lst = [ra4,ra2,ra5,ra3,ra1]     # Initialize the list
+
+        random.seed(4)                  # Set random number generation seed
+
+        lst.sort()
+        self.assertEqual(lst,[ra5,ra2,ra1,ra4,ra3])
+        lst.sort()
+        self.assertEqual(lst,[ra5,ra2,ra1,ra4,ra3])
+        lst.sort()
+        self.assertEqual(lst,[ra5,ra1,ra2,ra4,ra3])
 
 if __name__ == '__main__':
     unittest.main()
