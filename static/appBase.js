54af76437c93e1c92a2fa52201ab9f733638580c
conzty01 <conzty01@luther.edu>
2019-02-04 02:34:01 -0600

    Modified callAPI so that it can also send POST messages

--- static/appBase.js
+++ static/appBase.js
@@ -8,7 +8,7 @@ AppBase.prototype.init = function () {
 
 }
 
-AppBase.prototype.callAPI = function (apiStr, params, fun) {
+AppBase.prototype.callAPI = function (apiStr, params, fun, m="GET") {
     // apiStr = remainder of the api address
     // params = data that is to be sent to the server
     //    fun = the function that will handle the returning data
@@ -17,17 +17,30 @@ AppBase.prototype.callAPI = function (apiStr, params, fun) {
     console.log("Calling API: " + apiStr);
     //console.log(params);
 
-	$.ajax({
-		url: appConfig.apiURL + apiStr,
-		method: "GET",
-        data: params
-    }).done(function(data) {
-        fun(data)
-        dfd.resolve();
-    }).error(function(data){
-        console.log("Ajax Error: ",data);
-    });
-
+    if (m === "GET") {
+        $.ajax({
+    		url: appConfig.apiURL + apiStr,
+    		method: "GET",
+            data: params
+        }).done(function(data) {
+            fun(data)
+            dfd.resolve();
+        }).error(function(data){
+            console.log("Ajax Error: ",data);
+        });
+    } else if (m === "POST") {
+        $.ajax({
+    		url: appConfig.apiURL + apiStr,
+    		method: "POST",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify(params)
+        }).done(function(data) {
+            fun(data)
+            dfd.resolve();
+        }).error(function(data){
+            console.log("Ajax Error: ",data);
+        });
+    }
     return dfd;
 
     // $.get(appConfig.apiURL + apiStr, params, fun);
