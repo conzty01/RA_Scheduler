450e41716bf61c143e4c8376b4f47da304d68c1a
conzty01 <conzty01@luther.edu>
2021-01-05 17:03:29 -0600

    Completed two additional tests

--- breaks/tests/breakBP_addBreakDuty_test.py
+++ breaks/tests/breakBP_addBreakDuty_test.py
@@ -2,33 +2,233 @@ from unittest.mock import MagicMock, patch
 from scheduleServer import app
 import unittest
 
+from helperFunctions.helperFunctions import stdRet, getCurSchoolYear
+
 
 class TestBreakBP_addBreakDuty(unittest.TestCase):
     def setUp(self):
+        # Set up a number of items that will be used for these tests.
+
+        # -- Mock the os.environ method so that we can create the server. --
+
+        # Helper Dict for holding the os.environ configuration
+        self.helper_osEnviron = {
+            "CLIENT_ID": "TEST CLIENT_ID",
+            "PROJECT_ID": "TEST PROJECT_ID",
+            "AUTH_URI": "TEST AUTH_URI",
+            "TOKEN_URI": "TEST TOKEN_URI",
+            "AUTH_PROVIDER_X509_CERT_URL": "TEST AUTH_PROVIDER_X509_CERT_URL",
+            "CLIENT_SECRET": "TEST CLIENT_SECRET",
+            "REDIRECT_URIS": "TEST1,TEST2,TEST3,TEST4",
+            "JAVASCRIPT_ORIGINS": "TEST5,TEST6",
+            "EXPLAIN_TEMPLATE_LOADING": "FALSE",
+            "LOG_LEVEL": "WARNING",
+            "USE_ADHOC": "FALSE",
+            "SECRET_KEY": "TEST SECRET KEY",
+            "OAUTHLIB_RELAX_TOKEN_SCOPE": "1",
+            "OAUTHLIB_INSECURE_TRANSPORT": "1",
+            "HOST_URL": "https://localhost:5000",
+            "DATABASE_URL": "postgres://ra_sched"
+        }
+
+        # Create a dictionary patcher for the os.environ method
+        self.patcher_osEnviron = patch.dict("os.environ",
+                                            self.helper_osEnviron)
+
+        # Start the os patchers (No mock object is returned since we used patch.dict())
+        self.patcher_osEnviron.start()
+
+        # -- Create an instance of ScheduleServer that we may test against. --
+
+        # Mark the application as being tested
         app.config["TESTING"] = True
-        app.config[""]
-        # Mock appGlobals
-        # Mock getCurSchoolYear
-        # Mock getAuth
-        # Mock stdRet
+        # Disable the login_required decorator
+        app.config["LOGIN_DISABLED"] = True
+        # Reinitialize the Login Manager to accept the new configuration
+        app.login_manager.init_app(app)
+        # Create the test server
+        self.server = app.test_client()
+
+        # -- Create a patcher for the getAuth() method from helperFunctions --
+        #     since we have disabled the login manager for testing
+
+        # First we must create an object for the auth_level that we can manipulate
+        #  as needed for the tests. By default, the auth_level is set to 1.
+        self.mocked_authLevel = MagicMock(return_value=1)
+
+        # In order for the authLevel to respond to __lt__, __gt__, and __eq__ calls,
+        #  we need to create lambda functions that can effectively implement the
+        #  respective magic methods.
+        self.mocked_authLevel_ltMock = lambda me, other: me.return_value < other
+        self.mocked_authLevel_gtMock = lambda me, other: me.return_value > other
+        self.mocked_authLevel_eqMock = lambda me, other: me.return_value == other
+
+        # We then set the auth_level mock to return the __lt__ Mock
+        self.mocked_authLevel.__lt__ = self.mocked_authLevel_ltMock
+        # We then set the auth_level mock to return the __gt__ Mock
+        self.mocked_authLevel.__gt__ = self.mocked_authLevel_ltMock
+        # We then set the auth_level mock to return the __eq__ Mock
+        self.mocked_authLevel.__eq__ = self.mocked_authLevel_ltMock
+
+        # Assemble all of the desired values into a dict object.
+        self.helper_getAuth = {
+            "uEmail": "test@email.com",
+            "ra_id": 1,
+            "name": "Test User",
+            "hall_id": 1,
+            "auth_level": self.mocked_authLevel,
+            "hall_name": "Test Hall"
+        }
+
+        # Create the patcher for the getAuth() method
+        self.patcher_getAuth = patch("breaks.breaks.getAuth", autospec=True)
+
+        # Start the patcher - mock returned
+        self.mocked_getAuth = self.patcher_getAuth.start()
+
+        # Configure the mocked_getAuth to return the helper_getAuth dictionary
+        self.mocked_getAuth.return_value = self.helper_getAuth
+
+        # -- Create a patcher for the appGlobals file --
+        self.patcher_appGlobals = patch("breaks.breaks.ag", autospec=True)
+
+        # Start the patcher - mock returned
+        self.mocked_appGlobals = self.patcher_appGlobals.start()
+
+        # Configure the mocked appGlobals as desired
+        self.mocked_appGlobals.baseOpts = {"HOST_URL": "https://localhost:5000"}
+        self.mocked_appGlobals.conn = MagicMock()
+        self.mocked_appGlobals.UPLOAD_FOLDER = "./static"
+        self.mocked_appGlobals.ALLOWED_EXTENSIONS = {"txt", "csv"}
 
     def tearDown(self):
-        pass
+        # Stop all of the patchers
+        self.patcher_getAuth.stop()
+        self.patcher_appGlobals.stop()
+        self.patcher_osEnviron.stop()
 
-    def test_whenCalledFromClient_acceptsExpectedParameters(self):
-        pass
+    def resetAuthLevel(self):
+        # This function serves to reset the auth_level of the session
+        #  to the default value which is 1.
+        self.mocked_authLevel.return_value = 1
 
     def test_whenPassedValidParams_returnsSuccessfulResponse(self):
-        pass
+        # Test to ensure that when an authorized remote client reaches this API
+        #  endpoint that endpoint accepts the following parameters and returns a
+        #  successful result:
+        #
+        #  id       <int> -  an integer representing the ra.id for the RA that should
+        #                     be assigned to the break duty.
+        #  pts      <int> -  an integer representing how many points the new break duty
+        #                     should be worth.
+        #  dateStr  <str> -  a string representing the date that the break duty should
+        #                     occur on.
+
+        # -- Arrange --
+
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_authLevel.reset_mock()
+        self.mocked_appGlobals.conn.reset_mock()
+
+        # Set the auth_level of this session to 2
+        self.mocked_authLevel.return_value = 2
+
+        # Configure the appGlobals.conn.cursor.execute mock to return different values
+        #  after subsequent calls.
+        self.mocked_appGlobals.conn.cursor().fetchone.side_effect = [
+            (1,),  # First call returns raID
+            (1, 1)  # Second call returns the dayID and monthID
+        ]
+
+        # -- Act --
+
+        resp = self.server.post("/breaks/api/addBreakDuty",
+                                json=dict(
+                                    id=1,
+                                    pts=1,
+                                    dateStr="2021-01-05"
+                                ),
+                                base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
+        # -- Assert --
+
+        # Assert that appGlobals.conn.cursor().execute was called three times
+        self.assertEqual(self.mocked_appGlobals.conn.cursor().execute.call_count, 3)
+
+        # Assert that the last time appGlobals.conn.cursor().execute was called,
+        #  it was an insert statement. Since this line is using triple-quote strings,
+        #  the whitespace must match exactly.
+        self.mocked_appGlobals.conn.cursor().execute.assert_called_with(
+            """INSERT INTO break_duties (ra_id, hall_id, month_id, day_id, point_val)
+                    VALUES (%s, %s, %s, %s, %s);""",
+            (1, 1, 1, 1, 1)
+        )
+
+        # Assert that appGlobals.conn.commit was called once
+        self.mocked_appGlobals.conn.commit.assert_called_once()
+
+        # Assert that appGlobals.conn.cursor().close was called
+        self.mocked_appGlobals.conn.cursor().close.assert_called_once()
+
+        # Assert that we received a json response
+        self.assertTrue(resp.is_json)
+
+        # Assert that we got a successful response
+        self.assertEqual(resp.json, stdRet(1, "successful"))
 
     def test_WithUnauthorizedUser_returnsNotAuthorizedResponse(self):
-        pass
+        # Test to ensure that when a user that is NOT authorized to reach this
+        #  endpoint, they receive a JSON response that indicates that they are
+        #  not authorized. An authorized user is a user that has an auth_level
+        #  of at least 2 (AHD).
+
+        # -- Arrange --
+
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_authLevel.reset_mock()
+
+        # Reset the auth_level to 1
+        self.resetAuthLevel()
+
+        # -- Act --
+
+        # Make a request to the desired API endpoint
+        resp = self.server.post("/breaks/api/addBreakDuty",
+                                data=dict(
+                                    id=1,
+                                    pts=1,
+                                    dateStr="2021-01-05"
+                                ),
+                                base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
+        # -- Assert --
+
+        # Assert that we received a json response
+        self.assertTrue(resp.is_json)
+
+        # Assert that the json is formatted as expected
+        self.assertEqual(resp.json, stdRet(-1, "NOT AUTHORIZED"))
+
+        # Assert that we received a 200 status code
+        self.assertEqual(resp.status_code, 200)
+
+        # Assert that no additional call to the DB was made
+        self.mocked_appGlobals.conn.cursor().execute.assert_not_called()
 
     def test_WhenPassedInvalidRA_returnsInvalidRAResult(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
         pass
 
     def test_WhenPassedInvalidDay_returnsInvalidDayResult(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
         pass
 
     def test_WhenPassedInvalidMonth_returnsInvalidMonthResult(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
         pass
