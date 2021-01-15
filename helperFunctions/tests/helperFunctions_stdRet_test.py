9a2aaad7451f06ec39903fc3763222681381bbb7
conzty01 <conzty01@luther.edu>
2021-01-14 22:26:21 -0600

    Completed unittests for stdRet function

---
+++ helperFunctions/tests/helperFunctions_stdRet_test.py
@@ -0,0 +1,76 @@
+from unittest.mock import MagicMock, patch
+import unittest
+
+from helperFunctions.helperFunctions import stdRet
+
+
+class TestHelperFunctions_getSchoolYear(unittest.TestCase):
+    def setUp(self):
+        # Set up a number of items that will be used for these tests.
+
+        # -- Mock the os.environ method so that we can create the server. --
+
+        # Helper Dict for holding the os.environ configuration
+        self.helper_osEnviron = {
+            "CLIENT_ID": "TEST CLIENT_ID",
+            "PROJECT_ID": "TEST PROJECT_ID",
+            "AUTH_URI": "TEST AUTH_URI",
+            "TOKEN_URI": "TEST TOKEN_URI",
+            "AUTH_PROVIDER_X509_CERT_URL": "TEST AUTH_PROVIDER_X509_CERT_URL",
+            "CLIENT_SECRET": "TEST CLIENT_SECRET",
+            "REDIRECT_URIS": "TEST1,TEST2,TEST3,TEST4",
+            "JAVASCRIPT_ORIGINS": "TEST5,TEST6",
+            "EXPLAIN_TEMPLATE_LOADING": "FALSE",
+            "LOG_LEVEL": "WARNING",
+            "USE_ADHOC": "FALSE",
+            "SECRET_KEY": "TEST SECRET KEY",
+            "OAUTHLIB_RELAX_TOKEN_SCOPE": "1",
+            "OAUTHLIB_INSECURE_TRANSPORT": "1",
+            "HOST_URL": "https://localhost:5000",
+            "DATABASE_URL": "postgres://ra_sched"
+        }
+
+        # Create a dictionary patcher for the os.environ method
+        self.patcher_osEnviron = patch.dict("os.environ",
+                                            self.helper_osEnviron)
+
+        # Start the os patchers (No mock object is returned since we used patch.dict())
+        self.patcher_osEnviron.start()
+
+        # -- Create a patcher for the appGlobals file --
+        self.patcher_appGlobals = patch("helperFunctions.helperFunctions.ag", autospec=True)
+
+        # Start the patcher - mock returned
+        self.mocked_appGlobals = self.patcher_appGlobals.start()
+
+        # Configure the mocked appGlobals as desired
+        self.mocked_appGlobals.baseOpts = {"HOST_URL": "https://localhost:5000"}
+        self.mocked_appGlobals.conn = MagicMock()
+        self.mocked_appGlobals.UPLOAD_FOLDER = "./static"
+
+    def tearDown(self):
+        # Stop all of the patchers
+        self.patcher_appGlobals.stop()
+        self.patcher_osEnviron.stop()
+
+    def test_alwaysReturnsResultAsExpectedDict(self):
+        # Test to ensure that when called, the function always
+        #  returns the provided data as a dictionary
+
+        # -- Arrange --
+
+        # Create the necessary variables for this test
+        desiredMessage = "TEST MESSAGE"
+        desiredStatus = 1
+
+        expectedResult = {"status":desiredStatus, "msg": desiredMessage}
+
+        # -- Act --
+
+        # Call the function
+        result = stdRet(desiredStatus, desiredMessage)
+
+        # -- Assert --
+
+        # Assert that we received the expected result
+        self.assertDictEqual(expectedResult, result)
