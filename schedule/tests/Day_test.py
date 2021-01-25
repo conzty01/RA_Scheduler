96b16c4be6e56deb0571cf3ac5e3753a2a809271
conzty01 <conzty01@luther.edu>
2021-01-25 15:43:37 -0600

    Added two tests for providing an RA list to the Day object

--- schedule/tests/Day_test.py
+++ schedule/tests/Day_test.py
@@ -2,6 +2,7 @@ from schedule.ra_sched import Day, RA
 from datetime import date
 import unittest
 
+
 class TestDayObject(unittest.TestCase):
     def setUp(self):
         s = []
@@ -12,9 +13,74 @@ class TestDayObject(unittest.TestCase):
 
         self.day = Day(date(2018,5,24),3,numDutySlots=n+1,ras=s)
 
-    def testIter(self):
-        for ra in self.day:
-            self.assertIsInstance(ra,RA)
+    def test_whenProvidedRAList_setsNumDutySlotsToLengthOfProvidedList(self):
+        # Test to ensure that when a list is provided as the ras parameter,
+        #  the Day object sets the number of duty slots to be the length of
+        #  the ra list.
+
+        # -- Arrange --
+
+        # The date to be used for this test
+        desiredDate = date(2021, 1, 25)
+
+        raList1 = [1]
+        raList2 = [1, 2]
+        raList3 = [1, 2, 3]
+        raList4 = [i for i in range(43)]
+
+        # -- Act --
+
+        # Create the Day objects used for this test
+        testDay1 = Day(desiredDate, 0, ras=raList1)
+        testDay2 = Day(desiredDate, 0, ras=raList2)
+        testDay3 = Day(desiredDate, 0, ras=raList3)
+        testDay4 = Day(desiredDate, 0, ras=raList4)
+
+        # -- Assert --
+
+        # Assert that the numDutySlots attribute is updated to be the length
+        #  of the provided RA list.
+        self.assertEqual(testDay1.numDutySlots, len(raList1))
+        self.assertEqual(testDay2.numDutySlots, len(raList2))
+        self.assertEqual(testDay3.numDutySlots, len(raList3))
+        self.assertEqual(testDay4.numDutySlots, len(raList4))
+
+    def test_whenIteratingOverDayObject_iteratesOverRAsOnDuty(self):
+        # Test to ensure that the __iter__ method of the Day Object
+        #  iterates over the RAs that are assigned for duty on that
+        #  day.
+
+        # -- Arrange --
+
+        # The date to be used for this test
+        desiredDate = date(2021, 1, 25)
+
+        # Create the expected RA list
+        expectedRAList = []
+        for i in range(25):
+            expectedRAList.append(
+                RA(
+                    "{}".format(i),
+                    "{}".format(i),
+                    i,
+                    1,
+                    desiredDate
+                )
+            )
+
+        # Create the Day object used for this test
+        testDay = Day(desiredDate, 0, ras=expectedRAList)
+
+        # -- Act/Assert --
+
+        # Iterate over the Day object
+        for i, ra in enumerate(testDay):
+            # Assert that we received an RA object
+            self.assertIsInstance(ra, RA)
+
+            # Assert that the RA object that we received
+            #  is the expected RA object
+            self.assertEqual(expectedRAList[i], ra)
 
     def testAddRA(self):
         ra1 = RA("R","E",99,1,date(2017,2,2))
