1dfcb7cb52a3a9891125fe25be21382bfc2d5df4
conzty01 <conzty01@luther.edu>
2021-01-26 16:00:59 -0600

    Added skeleton for additional schedule module testing

--- schedule/tests/raSched_RAObject_test.py
+++ schedule/tests/raSched_RAObject_test.py
@@ -1,43 +1,73 @@
 from schedule.ra_sched import RA
+from unittest.mock import patch
 from datetime import date
 import unittest
 import random
 
+
 class TestRAObject(unittest.TestCase):
     def setUp(self):
-        self.ra = RA("Tyler","Conzett",1234,4321,date(2017,1,1),
-                     [date(2018,5,3),date(2018,5,7),date(2018,5,22)])
 
-    def testIter(self):
-        for pos,conflict in enumerate(self.ra):
-            self.assertEqual(self.ra.conflicts[pos],conflict)
+        # -- Create a patchers for the logging --
+        self.patcher_loggingDEBUG = patch("logging.debug", autospec=True)
+        self.patcher_loggingINFO = patch("logging.info", autospec=True)
+        self.patcher_loggingWARNING = patch("logging.warning", autospec=True)
+        self.patcher_loggingCRITICAL = patch("logging.critical", autospec=True)
+        self.patcher_loggingERROR = patch("logging.error", autospec=True)
 
-    def testGetConflicts(self):
-        self.assertEqual(self.ra.getConflicts(),self.ra.conflicts)
+        # Start the patcher - mock returned
+        self.mocked_loggingDEBUG = self.patcher_loggingDEBUG.start()
+        self.mocked_loggingINFO = self.patcher_loggingINFO.start()
+        self.mocked_loggingWARNING = self.patcher_loggingWARNING.start()
+        self.mocked_loggingCRITICAL = self.patcher_loggingCRITICAL.start()
+        self.mocked_loggingERROR = self.patcher_loggingERROR.start()
 
-    def testGetId(self):
-        self.assertEqual(self.ra.getId(),self.ra.id)
+        self.ra = RA("Tyler", "Conzett", 1234, 4321, date(2017, 1, 1),
+                     [date(2018, 5, 3), date(2018, 5, 7), date(2018, 5, 22)])
 
-    def testGetStartDate(self):
-        self.assertEqual(self.ra.getStartDate(),self.ra.dateStarted)
+    def tearDown(self):
+        # Stop all of the patchers
+        self.patcher_loggingDEBUG.stop()
+        self.patcher_loggingINFO.stop()
+        self.patcher_loggingWARNING.stop()
+        self.patcher_loggingCRITICAL.stop()
+        self.patcher_loggingERROR.stop()
 
-    def testGetPoints(self):
-        self.assertEqual(self.ra.getPoints(),self.ra.points)
+    def test_hasExpectedMethods(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
-    def testAddPoints(self):
-        prevPoints = self.ra.getPoints()
-        self.ra.addPoints(1)
-        newPoints = self.ra.getPoints()
+    def test_hasExpectedProperties(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
-        self.assertEqual(newPoints,prevPoints + 1)
+    def test_hasExpectedDefaultValues(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
-    def testGetName(self):
-        self.assertEqual(self.ra.getName(),self.ra.fullName)
+    def test_magicMethodStr_returnsExpectedString(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
-    def testGetHallId(self):
-        self.assertEqual(self.ra.getHallId(),self.ra.hallId)
+    def test_magicMethodRepr_returnsExpectedRepr(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
-    def testEqual(self):
+    def test_magicMethodIter_iteratesOverConflicts(self):
+        for pos, conflict in enumerate(self.ra):
+            self.assertEqual(self.ra.conflicts[pos], conflict)
+
+    def test_magicMethodEq_returnsTrueIfAndOnlyIfAllDesiredAttributesMatch(self):
         ra1 = RA("1","1",1,0,date(2017,1,1))
         ra2 = RA("1","1",1,0,date(2017,1,1))
         ra3 = RA("2","2",3,0,date(2017,1,1))
@@ -45,12 +75,18 @@ class TestRAObject(unittest.TestCase):
         self.assertEqual(ra1,ra2)
         self.assertNotEqual(ra1,ra3)
 
-    def testLessThan(self):
-        ra1 = RA("1","1",1,0,date(2017,1,1))
-        ra2 = RA("2","2",2,0,date(2017,1,1))
-        ra3 = RA("3","3",3,0,date(2017,1,1))
-        ra4 = RA("4","4",3,0,date(2017,1,1))
-        ra5 = RA("5","5",3,0,date(2017,1,1))
+    def test_magicMethodHash_returnsHashOfTupleOfAttributes(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_magicMethodLt_ifRAObjectsHaveUnequalPoints_evaluatesAppropriately(self):
+        ra1 = RA("1", "1", 1, 0, date(2017, 1, 1))
+        ra2 = RA("2", "2", 2, 0, date(2017, 1, 1))
+        ra3 = RA("3", "3", 3, 0, date(2017, 1, 1))
+        ra4 = RA("4", "4", 3, 0, date(2017, 1, 1))
+        ra5 = RA("5", "5", 3, 0, date(2017, 1, 1))
 
         # By default, all RAs have 0 points.
         ra1.addPoints(1)
@@ -60,16 +96,60 @@ class TestRAObject(unittest.TestCase):
         # The general, expected outcome is:
         #     ra5, ra1 or ra2, ra4, ra3
 
-        lst = [ra4,ra2,ra5,ra3,ra1]     # Initialize the list
+        lst = [ra4, ra2, ra5, ra3, ra1]  # Initialize the list
 
-        random.seed(4)                  # Set random number generation seed
+        random.seed(4)  # Set random number generation seed
 
         lst.sort()
-        self.assertEqual(lst,[ra5,ra2,ra1,ra4,ra3])
+        self.assertEqual(lst, [ra5, ra2, ra1, ra4, ra3])
         lst.sort()
-        self.assertEqual(lst,[ra5,ra2,ra1,ra4,ra3])
+        self.assertEqual(lst, [ra5, ra2, ra1, ra4, ra3])
         lst.sort()
-        self.assertEqual(lst,[ra5,ra1,ra2,ra4,ra3])
+        self.assertEqual(lst, [ra5, ra1, ra2, ra4, ra3])
+
+    def test_magicMethodLt_ifRAObjectsHaveEqualPoints_evaluatesRandomly(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_magicMethodDeepcopy_returnsDeepCopyOfRAObject(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_getConflicts_returnsConflictsAttribute(self):
+        self.assertEqual(self.ra.getConflicts(), self.ra.conflicts)
+
+    def test_getId_returnsIDAttribute(self):
+        self.assertEqual(self.ra.getId(), self.ra.id)
+
+    def test_getStartDate_returnsStartDateAttribute(self):
+        self.assertEqual(self.ra.getStartDate(), self.ra.dateStarted)
+
+    def test_getPoints_returnsPointsAttribute(self):
+        self.assertEqual(self.ra.getPoints(), self.ra.points)
+
+    def test_addPoints_addsProvidedNumberOfPointsToPointsAttribute(self):
+        prevPoints = self.ra.getPoints()
+        self.ra.addPoints(1)
+        newPoints = self.ra.getPoints()
+
+        self.assertEqual(newPoints, prevPoints + 1)
+
+    def test_removePoints_decreasesPointsAttributeByProvidedAmount(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_getName_returnsFullNameAttribute(self):
+        self.assertEqual(self.ra.getName(), self.ra.fullName)
+
+    def test_getHallId_returnsHallIDAttribute(self):
+        self.assertEqual(self.ra.getHallId(), self.ra.hallId)
+
 
 if __name__ == '__main__':
     unittest.main()
--- schedule/tests/raSched_ScheduleObject_test.py
+++ schedule/tests/raSched_ScheduleObject_test.py
@@ -1,48 +1,153 @@
 from schedule.ra_sched import Schedule, RA
+from unittest.mock import patch
 from datetime import date
 import unittest
 
+
 class TestScheduleObject(unittest.TestCase):
     def setUp(self):
-        self.sched = Schedule(2018,5,
-        noDutyDates=[date(2018,5,24),date(2018,5,25),date(2018,5,26)],
-        doubleDays=(4,5),
-        doubleDates=[date(2018,5,21),date(2018,5,22),date(2018,5,23)])
+        # -- Create a patchers for the logging --
+        self.patcher_loggingDEBUG = patch("logging.debug", autospec=True)
+        self.patcher_loggingINFO = patch("logging.info", autospec=True)
+        self.patcher_loggingWARNING = patch("logging.warning", autospec=True)
+        self.patcher_loggingCRITICAL = patch("logging.critical", autospec=True)
+        self.patcher_loggingERROR = patch("logging.error", autospec=True)
 
-    def testIter(self):
-        for pos,day in enumerate(self.sched):
-            self.assertEqual(self.sched.schedule[pos],day)
+        # Start the patcher - mock returned
+        self.mocked_loggingDEBUG = self.patcher_loggingDEBUG.start()
+        self.mocked_loggingINFO = self.patcher_loggingINFO.start()
+        self.mocked_loggingWARNING = self.patcher_loggingWARNING.start()
+        self.mocked_loggingCRITICAL = self.patcher_loggingCRITICAL.start()
+        self.mocked_loggingERROR = self.patcher_loggingERROR.start()
 
-    def testNumDays(self):
+        self.sched = Schedule(
+            2018,
+            5,
+            noDutyDates=[24, 25, 26],
+            doubleDays=(4, 5),
+            doubleDates=[21, 22, 23]
+        )
+
+    def tearDown(self):
+        # Stop all of the patchers
+        self.patcher_loggingDEBUG.stop()
+        self.patcher_loggingINFO.stop()
+        self.patcher_loggingWARNING.stop()
+        self.patcher_loggingCRITICAL.stop()
+        self.patcher_loggingERROR.stop()
+
+    def test_hasExpectedMethods(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_hasExpectedProperties(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_hasExpectedDefaultValues(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_withNoProvidedSchedule_generatesScheduleUsingDoubleDaysAndDates(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_withNoProvidedSchedule_generatesScheduleExcludingNoDutyDates(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_magicMethodRepr_returnsExpectedString(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_magicMethodIter_iteratesOverSchedule(self):
+        for pos, day in enumerate(self.sched):
+            self.assertEqual(self.sched.schedule[pos], day)
+
+    def test_magicMethodLen_returnsLengthOfSchedule(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_sort_sortsScheduleInDescendingOrder(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_numDays_returnsNumberOfDaysInSchedule(self):
         self.assertEqual(len(self.sched.schedule),self.sched.numDays())
 
-    def testGetDate(self):
-        self.assertEqual(self.sched.getDate(1),self.sched.schedule[0])
-        self.assertEqual(self.sched.getDate(self.sched.numDays()),self.sched.schedule[self.sched.numDays()-1])
-        self.assertRaises(IndexError,self.sched.getDate,0)
-        self.assertRaises(IndexError,self.sched.getDate,100)
+    def test_getDate_withValidIndex_returnsExpectedDayObject(self):
+        self.assertEqual(self.sched.getDate(1), self.sched.schedule[0])
+        self.assertEqual(self.sched.getDate(self.sched.numDays()), self.sched.schedule[self.sched.numDays() - 1])
+        self.assertRaises(IndexError, self.sched.getDate, 0)
+        self.assertRaises(IndexError, self.sched.getDate, 100)
 
-    def testAddRA(self):
-        ra = RA("T","C",1234,4321,date(2017,1,1))
+    def test_getDateWithInvalidIndex_throwsIndexError(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_addRA_assignsProvidedRAToDutyOnProvidedDate(self):
+        ra = RA("T", "C", 1234, 4321, date(2017, 1, 1))
         d = 1
         preAdd = self.sched.getDate(d).getRAs()
-        self.sched.addRA(d,ra)
+        self.sched.addRA(d, ra)
         postAdd = self.sched.getDate(d).getRAs()
 
-        self.assertEqual(preAdd,postAdd)
-        self.assertRaises(IndexError,self.sched.addRA,0,ra)
-        self.assertRaises(IndexError,self.sched.addRA,100,ra)
+        self.assertEqual(preAdd, postAdd)
+        self.assertRaises(IndexError, self.sched.addRA, 0, ra)
+        self.assertRaises(IndexError, self.sched.addRA, 100, ra)
 
-    def testRemoveRA(self):
-        ra = RA("R","K",5,1,date(2017,2,2))
+    def test_removeRA_removesProvidedRAFromProvidedDuty(self):
+        ra = RA("R", "K", 5, 1, date(2017, 2, 2))
         d = 1
 
         preRem = self.sched.getDate(d).getRAs()
-        self.sched.addRA(d,ra)
-        self.sched.removeRA(d,ra)
+        self.sched.addRA(d, ra)
+        self.sched.removeRA(d, ra)
         postRem = self.sched.getDate(d).getRAs()
 
-        self.assertEqual(preRem,postRem)
+        self.assertEqual(preRem, postRem)
+
+    def test_setReview_setsReviewAttributeToTrue(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_addReviewDay_addsProvidedDayToReviewDaysAttribute(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_getReviewDays_returnsReviewDaysAttribute(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
+
+    def test_shouldReview_returnsValueOfReviewAttribute(self):
+        # -- Arrange --
+        # -- Act --
+        # -- Assert --
+        pass
 
 
 if __name__ == "__main__":
