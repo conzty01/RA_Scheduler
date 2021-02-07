f8436fba6a79899f09bff7828096b869ef552e0a
conzty01 <conzty01@luther.edu>
2021-02-07 13:05:23 -0600

    Completed unit tests for changeHallView API

--- staff/tests/staff_changeHallView_test.py
+++ staff/tests/staff_changeHallView_test.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock, patch
 from scheduleServer import app
+from flask import Response
 import unittest
 
 from helperFunctions.helperFunctions import AuthenticatedUser
@@ -73,11 +74,17 @@ class TestHallBP_getHallSettings(unittest.TestCase):
         # Set the ra_id and hall_id to values that can be used throughout
         self.user_ra_id = 1
         self.user_hall_id = 1
+        self.user_hall_id2 = 2
         self.associatedResHalls = [
             {
                 "id": self.user_hall_id,
                 "auth_level": self.mocked_authLevel,
                 "name": "Test Hall"
+            },
+            {
+                "id": self.user_hall_id2,
+                "auth_level": self.mocked_authLevel,
+                "name": "Test Hall 2"
             }
         ]
 
@@ -91,7 +98,7 @@ class TestHallBP_getHallSettings(unittest.TestCase):
         )
 
         # Create the patcher for the getAuth() method
-        self.patcher_getAuth = patch("hall.hall.getAuth", autospec=True)
+        self.patcher_getAuth = patch("staff.staff.getAuth", autospec=True)
 
         # Start the patcher - mock returned
         self.mocked_getAuth = self.patcher_getAuth.start()
@@ -100,7 +107,7 @@ class TestHallBP_getHallSettings(unittest.TestCase):
         self.mocked_getAuth.return_value = self.helper_getAuth
 
         # -- Create a patcher for the appGlobals file --
-        self.patcher_appGlobals = patch("hall.hall.ag", autospec=True)
+        self.patcher_appGlobals = patch("staff.staff.ag", autospec=True)
 
         # Start the patcher - mock returned
         self.mocked_appGlobals = self.patcher_appGlobals.start()
@@ -142,26 +149,196 @@ class TestHallBP_getHallSettings(unittest.TestCase):
         #  to the default value which is 1.
         self.mocked_authLevel.return_value = 1
 
-    def test_whenProvidedHallUserIsAssociatedWith_setsProvidedHallAsSelected(self):
-        # -- Arrange --
-        # -- Act --
-        # -- Assert --
-        self.fail("Not Implemented")
+    @patch("staff.staff.redirect", autospec=True)
+    @patch("staff.staff.url_for", autospec=True)
+    def test_whenProvidedHallUserIsAssociatedWith_setsProvidedHallAsSelected(self, mocked_urlFor, mocked_redirect):
+        # Test to ensure that when the method is provided a hall that the user is
+        #  associated with, the method sets the provided hall as the selected hall.
 
-    def test_whenProvidedHallUserIsAssociatedWith_returnsRedirectToIndex(self):
         # -- Arrange --
-        # -- Act --
-        # -- Assert --
-        self.fail("Not Implemented")
 
-    def test_whenProvidedHallUserIsNOTAssociatedWith_doesNotInteractWithDB(self):
-        # -- Arrange --
-        # -- Act --
-        # -- Assert --
-        self.fail("Not Implemented")
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_appGlobals.conn.reset_mock()
 
-    def test_whenProvidedHallUserIsNOTAssociatedWith_returnsRedirectToIndex(self):
-        # -- Arrange --
+        # Set the various items to be used in this test
+        desiredHallID = self.user_hall_id2
+
+        # Configure the mocked redirect function to return a valid response object
+        mocked_redirect.return_value = Response(status=200)
+
         # -- Act --
+
+        # Make a request to the desired API endpoint
+        resp = self.server.get("/staff/api/changeHallView/{}".format(desiredHallID),
+                               base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
         # -- Assert --
-        self.fail("Not Implemented")
+
+        # Assert that the when the appGlobals.conn.cursor().execute was called,
+        #  it was an UPDATE statement to clear out the selected halls for the user.
+        self.mocked_appGlobals.conn.cursor().execute.assert_any_call(
+            """
+                UPDATE staff_membership
+                SET selected = false
+                WHERE ra_id = %s
+            """
+            , (self.user_ra_id, )
+        )
+
+        # Assert that the when the appGlobals.conn.cursor().execute was called,
+        #  it was an UPDATE statement to mark the desired hall as selected.
+        self.mocked_appGlobals.conn.cursor().execute.assert_any_call(
+            """
+                UPDATE staff_membership
+                SET selected = true
+                WHERE ra_id = %s
+                AND res_hall_id = %s
+            """
+            , (self.user_ra_id, desiredHallID)
+        )
+
+        # Assert that appGlobals.conn.commit was called
+        self.mocked_appGlobals.conn.commit.assert_called_once()
+
+        # Assert that appGlobals.conn.cursor().close was called
+        self.mocked_appGlobals.conn.cursor().close.assert_called_once()
+
+        # Assert that the url_for was called for the index page
+        mocked_urlFor.assert_called_once_with("index")
+
+        # Assert that a redirect was created for the index page
+        mocked_redirect.assert_called_once_with(mocked_urlFor("index"))
+
+    @patch("staff.staff.redirect", autospec=True)
+    @patch("staff.staff.url_for", autospec=True)
+    def test_whenProvidedHallUserIsAssociatedWith_returnsRedirectToIndex(self, mocked_urlFor, mocked_redirect):
+        # Test to ensure that when provided a hall ID that the user is associated
+        #  with, the method returns a redirect to the index page.
+
+        # -- Arrange --
+
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_appGlobals.conn.reset_mock()
+
+        # Set the various items to be used in this test
+        desiredHallID = self.user_hall_id2
+
+        # Configure the mocked redirect function to return a valid response object
+        mocked_redirect.return_value = Response(status=200)
+
+        # -- Act --
+
+        # Make a request to the desired API endpoint
+        resp = self.server.get("/staff/api/changeHallView/{}".format(desiredHallID),
+                               base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
+        # -- Assert --
+
+        # Assert that the when the appGlobals.conn.cursor().execute was called,
+        #  it was an UPDATE statement to clear out the selected halls for the user.
+        self.mocked_appGlobals.conn.cursor().execute.assert_any_call(
+            """
+                UPDATE staff_membership
+                SET selected = false
+                WHERE ra_id = %s
+            """
+            , (self.user_ra_id,)
+        )
+
+        # Assert that the when the appGlobals.conn.cursor().execute was called,
+        #  it was an UPDATE statement to mark the desired hall as selected.
+        self.mocked_appGlobals.conn.cursor().execute.assert_any_call(
+            """
+                UPDATE staff_membership
+                SET selected = true
+                WHERE ra_id = %s
+                AND res_hall_id = %s
+            """
+            , (self.user_ra_id, desiredHallID)
+        )
+
+        # Assert that appGlobals.conn.commit was called
+        self.mocked_appGlobals.conn.commit.assert_called_once()
+
+        # Assert that appGlobals.conn.cursor().close was called
+        self.mocked_appGlobals.conn.cursor().close.assert_called_once()
+
+        # Assert that the url_for was called for the index page
+        mocked_urlFor.assert_called_once_with("index")
+
+        # Assert that a redirect was created for the index page
+        mocked_redirect.assert_called_once_with(mocked_urlFor("index"))
+
+    @patch("staff.staff.redirect", autospec=True)
+    @patch("staff.staff.url_for", autospec=True)
+    def test_whenProvidedHallUserIsNOTAssociatedWith_doesNotInteractWithDB(self, mocked_urlFor, mocked_redirect):
+        # Test to ensure that when the provided hall id is not associated with the user,
+        #  the method does not interact with the DB.
+
+        # -- Arrange --
+
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_appGlobals.conn.reset_mock()
+
+        # Set the various items to be used in this test
+        desiredHallID = 999
+
+        # Configure the mocked redirect function to return a valid response object
+        mocked_redirect.return_value = Response(status=200)
+
+        # -- Act --
+
+        # Make a request to the desired API endpoint
+        resp = self.server.get("/staff/api/changeHallView/{}".format(desiredHallID),
+                               base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
+        # -- Assert --
+
+        # Assert that appGlobals.conn.cursor.execute was never called
+        self.mocked_appGlobals.conn.cursor().execute.assert_not_called()
+
+        # Assert that appGlobals.conn.commit was never called
+        self.mocked_appGlobals.conn.commit.assert_not_called()
+
+        # Assert that appGlobals.conn.cursor().close was called
+        self.mocked_appGlobals.conn.cursor().close.assert_called_once()
+
+    @patch("staff.staff.redirect", autospec=True)
+    @patch("staff.staff.url_for", autospec=True)
+    def test_whenProvidedHallUserIsNOTAssociatedWith_returnsRedirectToIndex(self, mocked_urlFor, mocked_redirect):
+        # Test to ensure that when the provided hall id is not associated with the user,
+        #  the method returns a redirect to the index page.
+
+        # -- Arrange --
+
+        # Reset all of the mocked objects that will be used in this test
+        self.mocked_appGlobals.conn.reset_mock()
+
+        # Set the various items to be used in this test
+        desiredHallID = 999
+
+        # Configure the mocked redirect function to return a valid response object
+        mocked_redirect.return_value = Response(status=200)
+
+        # -- Act --
+
+        # Make a request to the desired API endpoint
+        resp = self.server.get("/staff/api/changeHallView/{}".format(desiredHallID),
+                               base_url=self.mocked_appGlobals.baseOpts["HOST_URL"])
+
+        # -- Assert --
+
+        # Assert that appGlobals.conn.cursor.execute was never called
+        self.mocked_appGlobals.conn.cursor().execute.assert_not_called()
+
+        # Assert that appGlobals.conn.commit was never called
+        self.mocked_appGlobals.conn.commit.assert_not_called()
+
+        # Assert that appGlobals.conn.cursor().close was called
+        self.mocked_appGlobals.conn.cursor().close.assert_called_once()
+
+        # Assert that the url_for was called for the index page
+        mocked_urlFor.assert_called_once_with("index")
+
+        # Assert that a redirect was created for the index page
+        mocked_redirect.assert_called_once_with(mocked_urlFor("index"))
