a577556b298cbab56fa715c4d1e63c0bb6147cd8
conzty01 <conzty01@luther.edu>
2021-02-11 15:53:16 -0600

    Updated the manStaff() method to abort with a 403 error if the user is not authorized

--- staff/staff.py
+++ staff/staff.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify, redirect, url_for, Blueprint
+from flask import render_template, request, jsonify, redirect, url_for, Blueprint, abort
 from flask_login import login_required
 from psycopg2 import IntegrityError
 import logging
@@ -37,8 +37,8 @@ def manStaff():
         # Log the occurrence.
         logging.info("User Not Authorized - RA: {}".format(authedUser.ra_id()))
 
-        # Notify the user that they are not authorized.
-        return jsonify(stdRet(-1, "NOT AUTHORIZED"))
+        # Raise an 403 Access Denied HTTP Exception that will be handled by flask
+        abort(403)
 
     # Get the information for the current school year.
     #  This will be used to calculate duty points for the RAs.
