3e4238d8ad11a0f380e6d19551f5930127f18af1
conzty01 <conzty01@luther.edu>
2021-06-01 22:04:53 -0500

    Added 'school_id' column to res_hall table which references the newly created school table.

--- migrateDB.py
+++ migrateDB.py
@@ -39,6 +39,73 @@ def migrate(conn):
 
         logging.info("  Finished creating scheduler_queue table")
 
+    # ---------------------------
+    # --  Create school table  --
+    # ---------------------------
+
+    # Check to see if the table already exists
+    cur.execute(
+        "SELECT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'school' AND schemaname = 'public');"
+    )
+
+    # If the table does not already exist...
+    if not cur.fetchone()[0]:
+        logging.info("  Creating school table")
+
+        # Create the scheduler_queue table
+        cur.execute("""
+            CREATE TABLE school(
+                id              serial UNIQUE,
+                name            varchar(255) NOT NULL,
+                city            varchar(255) NOT NULL,
+                state           varchar(2) NOT NULL,
+                
+                PRIMARY KEY (name, city, state)
+        );""")
+
+        logging.info("  Finished creating school table")
+
+        logging.info("  Adding RADSA Support info into school table")
+
+        # Add a record for RADSA support into the school table for
+        cur.execute("INSERT INTO school (name, city, state) VALUES ('ConzTech', 'Deco', 'IA');")
+
+        logging.info("  Finished adding RADSA Support info into school table")
+
+    # ----------------------------------------------
+    # --  Add school_id column to res_hall table  --
+    # ----------------------------------------------
+
+    # Check to see if the column already exists
+    cur.execute(
+        """SELECT EXISTS (
+            SELECT column_name 
+            FROM information_schema.columns 
+            WHERE table_name='res_hall' 
+            AND column_name='school_id');"""
+    )
+
+    # If the column does not already exist...
+    if not cur.fetchone()[0]:
+        logging.info("  Adding school_id column to res_hall table")
+
+        # Create the column in the res_hall.
+        cur.execute("""
+            ALTER TABLE res_hall
+            ADD COLUMN school_id int
+            ;""")
+
+        # Set a default value for the school_id column so that we can set it as a foreign key
+        cur.execute("UPDATE res_hall SET school_id = (SELECT id FROM school LIMIT(1));")
+
+        # Set the school_id column as a foreign key to the school table
+        cur.execute("ALTER TABLE res_hall ADD FOREIGN KEY (school_id) REFERENCES school(id);")
+
+        # Set the school_id column to not allow Null values
+        cur.execute("ALTER TABLE res_hall ALTER COLUMN school_id SET NOT NULL;")
+
+        logging.info("  Finished adding school_id column to res_hall table")
+
 
 if __name__ == "__main__":
 
