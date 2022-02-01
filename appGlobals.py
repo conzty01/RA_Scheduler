0e0142e883e11bd7e00465f81de0672499eeb979
conzty01 <conzty01@luther.edu>
2022-01-31 19:33:46 -0600

    Created RADSADatabaseCursor class to wrap the psycopg2 cursor object

--- appGlobals.py
+++ appGlobals.py
@@ -1,5 +1,25 @@
-import psycopg2
+from psycopg2.extensions import cursor
+from psycopg2 import connect
+import logging
 import os
+
+
+class RADSADatabaseCursor(cursor):
+    """ Object for wrapping the psycopg2 cursor class for RADSA. """
+
+    def execute(self, sql, args=None):
+
+        try:
+            # Execute the provided SQL
+            cursor.execute(self, sql, args)
+
+        except Exception as e:
+            # Log the occurrence
+            logging.exception("Error encountered querying database - {}:{}".format(e.__class__.__name__, e))
+
+            # Rollback the transaction
+            self.connection.rollback()
+
 
 # Set up baseOpts to be sent to each HTML template
 baseOpts = {
@@ -7,7 +27,10 @@ baseOpts = {
 }
 
 # Establish DB connection
-conn = psycopg2.connect(os.getenv("DATABASE_URL", "postgres:///ra_sched"))
+conn = connect(
+    os.getenv("DATABASE_URL", "postgres:///ra_sched"),
+    cursor_factory=RADSADatabaseCursor
+)
 
 # Set up various global variables
 UPLOAD_FOLDER = "./static"
